4e69a4c7600c58feac127869c8c8a6c9bd9cc4f0
Nels Nelson <nels.n.nelson@gmail.com>
2014-10-03 12:29:07 -0500

    quick update

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,8 +5,9 @@ export DEBIAN_FRONTEND=noninteractive
 echo "debconf debconf/frontend select Noninteractive" | debconf-set-selections
 apt-get upgrade -o Dpkg::Options::="--force-confnew" --assume-yes --fix-missing
 apt-get install --assume-yes --fix-missing git
-pushd /tmp
-git clone git@github.rackspace.com:team-containers/containers-devstack-tempest/scripts
+git clone https://github.com/nelsnelson/containers-devstack-tempest.git /tmp/a
+mv /tmp/a/scripts /tmp/scripts
+rm -rf /tmp/a
 /tmp/scripts/jenkins-user.sh
 cp /tmp/scripts/jenkins-devstack-env.sh /home/jenkins/scripts
 cp /tmp/scripts/jenkins-devstack.sh /home/jenkins/scripts
