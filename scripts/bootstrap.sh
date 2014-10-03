dadf97c2971770c03541cd3c98b3cbedef8e7d2b
Nels Nelson <nels.n.nelson@gmail.com>
2014-10-03 12:32:33 -0500

    quick update

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,10 +6,6 @@ echo "debconf debconf/frontend select Noninteractive" | debconf-set-selections
 apt-get upgrade -o Dpkg::Options::="--force-confnew" --assume-yes --fix-missing
 apt-get install --assume-yes --fix-missing git
 git clone https://github.com/nelsnelson/containers-devstack-tempest.git /tmp/a
-mv /tmp/a/scripts /tmp/scripts
-rm -rf /tmp/a
-/tmp/scripts/jenkins-user.sh
-cp /tmp/scripts/jenkins-devstack-env.sh /home/jenkins/scripts
-cp /tmp/scripts/jenkins-devstack.sh /home/jenkins/scripts
-/tmp/scripts/openstack-infra-install.sh
+/tmp/a/scripts/jenkins-user.sh
+/tmp/a/scripts/openstack-infra-install.sh
 
--- scripts/jenkins-user.sh
+++ scripts/jenkins-user.sh
@@ -3,11 +3,11 @@
 adduser --quiet --disabled-password --gecos '' jenkins
 mkdir -p /etc/sudoers.d
 echo "jenkins ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/admin
-#mkdir -p /home/jenkins/.ssh
-#cp /root/.ssh/authorized_keys /home/jenkins/.ssh/
-#chown -R jenkins: /home/jenkins/.ssh
-#mkdir -p /home/jenkins/scripts
-#mv /root/scripts/jenkins-devstack-env.sh /home/jenkins/scripts/jenkins-devstack-env.sh
-#mv /root/scripts/jenkins-devstack.sh /home/jenkins/scripts/jenkins-devstack.sh
-#chown -R jenkins: /home/jenkins/scripts
+mkdir -p /home/jenkins/.ssh
+cp /root/.ssh/authorized_keys /home/jenkins/.ssh/
+chown -R jenkins: /home/jenkins/.ssh
+mkdir -p /home/jenkins/scripts
+cp /tmp/a/scripts/jenkins-devstack-env.sh /home/jenkins/scripts
+cp /tmp/a/scripts/jenkins-devstack.sh /home/jenkins/scripts
+chown -R jenkins: /home/jenkins/scripts
 
