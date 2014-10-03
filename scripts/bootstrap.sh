b381f92969617e5007e6cf2a24fdfcde65dd66fd
Nels Nelson <nels.n.nelson@gmail.com>
2014-10-03 11:40:09 -0500

    quick update

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,9 +6,9 @@ echo "debconf debconf/frontend select Noninteractive" | debconf-set-selections
 apt-get upgrade -o Dpkg::Options::="--force-confnew" --assume-yes --fix-missing
 apt-get install --assume-yes --fix-missing git
 pushd /tmp
-git clone git@github.rackspace.com:team-containers/containers-devstack-tempest.git
-/tmp/containers-devstack-tempest/scripts/jenkins-user.sh
-cp /tmp/containers-devstack-tempest/scripts/jenkins-devstack-env.sh /home/jenkins/scripts
-cp /tmp/containers-devstack-tempest/scripts/jenkins-devstack.sh /home/jenkins/scripts
-/tmp/containers-devstack-tempest/scripts/openstack-infra-install.sh
+git clone git@github.rackspace.com:team-containers/containers-devstack-tempest/scripts
+/tmp/scripts/jenkins-user.sh
+cp /tmp/scripts/jenkins-devstack-env.sh /home/jenkins/scripts
+cp /tmp/scripts/jenkins-devstack.sh /home/jenkins/scripts
+/tmp/scripts/openstack-infra-install.sh
 
