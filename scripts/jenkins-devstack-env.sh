2f37480b894aa9091a4d81bc7e9d923eddcf09d7
Nels Nelson <nels.n.nelson@gmail.com>
2014-10-03 10:18:17 -0500

    quick update

---
+++ scripts/jenkins-devstack-env.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+export REPO_URL=https://review.openstack.org/p
+export ZUUL_URL=/home/jenkins/workspace-cache
+export ZUUL_REF=HEAD
+export WORKSPACE=/home/jenkins/workspace/testing
+export ZUUL_PROJECT=openstack/nova
+export ZUUL_BRANCH=master
+
---
+++ scripts/jenkins-devstack.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+source ./jenkins-devstack-env.sh
+
+mkdir -p $WORKSPACE
+git clone $REPO_URL/$ZUUL_PROJECT $ZUUL_URL/$ZUUL_PROJECT
+pushd $ZUUL_URL/$ZUUL_PROJECT
+git checkout remotes/origin/$ZUUL_BRANCH
+popd
+pushd $WORKSPACE
+git clone --depth 1 $REPO_URL/openstack-infra/devstack-gate
+popd
+
