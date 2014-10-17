0d4df8bd72af0c3d498776f6a79d879a9591ebee
Nels Nelson <nels.n.nelson@gmail.com>
2014-10-17 14:47:41 -0500

    quick update

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,24 +4,12 @@
 # add user "jenkins" to sudoers, install pip packages, add tox via pip 
 # (because the packaged version is too old) and reboot to make sure you're 
 # running a current kernel:
-
-# Upgrade the server
 apt-get update --fix-missing
 export DEBIAN_FRONTEND=noninteractive
 echo "debconf debconf/frontend select Noninteractive" | debconf-set-selections
 apt-get upgrade -o Dpkg::Options::="--force-confnew" --assume-yes --fix-missing
-
-# Install git
 apt-get install --assume-yes --fix-missing git
-
-# Clone the containers-devstack-tempest repo to get the
-# rest of the installation scripts
 git clone https://github.com/nelsnelson/containers-devstack-tempest.git /tmp/a
-
-# Set up a "jenkins" account and add user "jenkins" to sudoers
 /tmp/a/scripts/jenkins-user.sh
-
-# Install pip packages, add tox via pip (because the
-# packaged version is too old)
 /tmp/a/scripts/openstack-infra-install.sh
 
