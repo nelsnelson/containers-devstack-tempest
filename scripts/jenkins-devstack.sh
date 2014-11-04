f5d916aaf53a03ec91ebfd56398405171ea19586
Nels Nelson <nels.n.nelson@gmail.com>
2014-11-04 09:18:11 -0600

    Trying to get tempest output

--- scripts/jenkins-devstack.sh
+++ scripts/jenkins-devstack.sh
@@ -18,6 +18,8 @@ export DEBIAN_FRONTEND=noninteractive
 echo "debconf debconf/frontend select Noninteractive" | debconf-set-selections
 export NTP_SERVER=pool.ntp.org
 rm $HOME/devstack-gate-log.txt
-`$WORKSPACE/safe-devstack-vm-gate-wrap.sh > /tmp/devstack-gate-log.txt` &
+mkdir -p $WORKSPACE/tmp
+`$WORKSPACE/safe-devstack-vm-gate-wrap.sh > $WORKSPACE/tmp/devstack-gate-log.txt` &
 wait ${!}
-mv /tmp/devstack-gate-log.txt $HOME/devstack-gate-log.txt
+mv $WORKSPACE/tmp/devstack-gate-log.txt $WORKSPACE/devstack-gate-log.txt
+rmdir $WORKSPACE/tmp
