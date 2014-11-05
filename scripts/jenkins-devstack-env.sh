b26b0caf2d683d04b0ed1484ea4b1c017bfed8ec
Nels Nelson <nels.n.nelson@gmail.com>
2014-11-05 15:18:53 -0600

    Trying to adjust devstack target

--- scripts/jenkins-devstack-env.sh
+++ scripts/jenkins-devstack-env.sh
@@ -1,11 +1,14 @@
 #! /usr/bin/env bash
 
 export REPO_URL=https://review.openstack.org/p
-export ZUUL_URL=/home/jenkins/workspace-cache
+#export ZUUL_URL=/home/jenkins/workspace-cache
+export ZUUL_URL=http://github.com
 export ZUUL_REF=HEAD
 export WORKSPACE=/home/jenkins/workspace/testing
-export ZUUL_PROJECT=openstack/nova
-export ZUUL_BRANCH=master
+#export ZUUL_PROJECT=openstack/nova
+export ZUUL_PROJECT=nelsnelson/devstack-gate
+#export ZUUL_BRANCH=master
+export ZUUL_BRANCH=libvirt_target
 export PYTHONUNBUFFERED=true
 export DEVSTACK_GATE_TIMEOUT=120
 export DEVSTACK_GATE_TEMPEST=1
