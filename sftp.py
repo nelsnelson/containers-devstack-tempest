522c69b975bf2171f71d56e51b07dbeda0e0fcd3
Nels Nelson <nels.n.nelson@gmail.com>
2014-11-10 19:54:31 -0600

    Trying to make sftp work

--- sftp.py
+++ sftp.py
@@ -4,7 +4,7 @@ import ssh
 
 import novaclient
 
-import client
+import get
 import logs
 import sys
 
@@ -17,26 +17,14 @@ def sftp(server, remote_path, user='root'):
     result = ssh.get(target, remote_path, user=user, keyfile=private_key)
     print result
 
-def server(id):
-    try:
-        log.info('Getting server with id {}'.format(id))
-        server = client.nova.servers.get(id)
-    except novaclient.openstack.common.apiclient.exceptions.NotFound as ex:
-        log.error('No server with id {}'.format(id))
-        sys.exit(1)
-    return server
-
 def main():
     if len(sys.argv) < 3:
-        print 'Please provide a host id'
-        sys.exit(0)
-    if len(sys.argv) < 2:
-        print 'Please provide a remote file to access'
+        print 'Usage:', __name__, ' <server_id> <remote_path>'
         sys.exit(0)
 
     id = sys.argv[1]
     file = sys.argv[2]
-    server = server(id)
+    server = get.server(id)
     sftp(server, file)
 
 if __name__ == '__main__':
