617a02630476916d8c8a7030db3b348bc68c9404
Nels Nelson <nels.n.nelson@gmail.com>
2014-12-02 13:36:15 -0600

    Trying to fix missing openstack-infra problem

--- scripts/openstack-infra-install.sh
+++ scripts/openstack-infra-install.sh
@@ -2,8 +2,10 @@
 
 mkdir -p /home/jenkins/src
 pushd /home/jenkins/src
-git clone https://review.openstack.org/p/openstack-infra/config
-config/install_puppet.sh && config/install_modules.sh
+#git clone https://review.openstack.org/p/openstack-infra/config
+git clone https://github.com/openstack-infra/system-config.git
+#config/install_puppet.sh && config/install_modules.sh
+system-config/install_puppet.sh && config/install_modules.sh
 puppet apply --modulepath=/root/config/modules:/etc/puppet/modules -e \
 "class { openstack_project::single_use_slave: install_users => false, ssh_key => \"$( cat $HOME/.ssh/id_rsa.pub | awk '{print $2}' )\" }"
 popd
