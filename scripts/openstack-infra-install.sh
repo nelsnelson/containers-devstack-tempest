3662780fc46ef8d27e85d8d3c085fe6c54f8c963
Nels Nelson <nels.n.nelson@gmail.com>
2014-12-04 11:42:00 -0600

    Trying to fix missing openstack-infra problem

--- scripts/openstack-infra-install.sh
+++ scripts/openstack-infra-install.sh
@@ -9,13 +9,9 @@ export NTP_SERVER=pool.ntp.org
 
 mkdir -p /home/jenkins/src
 pushd /home/jenkins/src
-#git clone https://review.openstack.org/p/openstack-infra/config
 git clone https://git.openstack.org/openstack-infra/system-config
-#git clone git://git.openstack.org/openstack/nova.git
-# git clone https://github.com/openstack-infra/system-config.git
-#config/install_puppet.sh && config/install_modules.sh
 system-config/install_puppet.sh && system-config/install_modules.sh
-puppet apply --modulepath=/root/config/modules:/etc/puppet/modules -e \
+puppet apply --modulepath=/root/system-config/modules:/etc/puppet/modules -e \
 "class { openstack_project::single_use_slave: install_users => false, ssh_key => \"$( cat $HOME/.ssh/id_rsa.pub | awk '{print $2}' )\" }"
 popd
 
