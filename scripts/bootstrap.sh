2245fe137fcbe185087144e50f9017a35ca43f42
Nels Nelson <nels.n.nelson@gmail.com>
2014-12-05 10:16:01 -0600

    Trying to fix some ssh paramiko problems

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,5 +11,5 @@ apt-get upgrade -o Dpkg::Options::="--force-confnew" --assume-yes --fix-missing
 apt-get install --assume-yes --fix-missing git
 git clone https://github.com/nelsnelson/containers-devstack-tempest.git /tmp/a
 /tmp/a/scripts/jenkins-user.sh
-/tmp/a/scripts/openstack-infra-install.sh
+#/tmp/a/scripts/openstack-infra-install.sh
 
