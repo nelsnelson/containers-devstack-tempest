7103c0bfa00c620dc6fa839e90a003daaab2665e
Nels Nelson <nels.n.nelson@gmail.com>
2014-12-05 12:06:44 -0600

    Trying to fix some ssh paramiko problems

--- scripts/openstack-infra-install.sh
+++ scripts/openstack-infra-install.sh
@@ -5,7 +5,7 @@ echo "server pool.ntp.org" > /etc/ntp.conf
 export NTP_SERVER=pool.ntp.org
 
 git clone https://git.openstack.org/openstack-infra/system-config /root/system-config
-#/root/system-config/install_puppet.sh && /root/system-config/install_modules.sh
+/root/system-config/install_puppet.sh && /root/system-config/install_modules.sh
 #puppet apply --modulepath=/root/system-config/modules:/etc/puppet/modules -e \
 #"class { openstack_project::single_use_slave: install_users => false, ssh_key => \"$( cat /root/.ssh/id_rsa.pub | awk '{print $2}' )\" }"
 
