f68f6b92fa5c3228ad842e10240f1acce5724f41
Nels Nelson <nels.n.nelson@gmail.com>
2014-12-09 13:33:46 -0600

    Trying to fix cpu pegging

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,7 +8,7 @@ apt-get update --fix-missing
 export DEBIAN_FRONTEND=noninteractive
 echo "debconf debconf/frontend select Noninteractive" | debconf-set-selections
 apt-get upgrade -o Dpkg::Options::="--force-confnew" --assume-yes --fix-missing
-apt-get install --assume-yes --fix-missing git
+apt-get install --assume-yes --fix-missing git screen
 git clone https://github.com/nelsnelson/containers-devstack-tempest.git /tmp/a
 /tmp/a/scripts/jenkins-user.sh
 /tmp/a/scripts/openstack-infra-install.sh
