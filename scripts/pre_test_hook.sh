62524c80ebd1006403f7aa931c1f9f547405a565
Nels Nelson <nels.n.nelson@gmail.com>
2014-12-15 11:34:30 -0600

    Testing out pre_test_hook tempest.conf adjustments

--- scripts/pre_test_hook.sh
+++ scripts/pre_test_hook.sh
@@ -8,18 +8,18 @@ pre_test_hook() {
     git clone https://github.com/nelsnelson/devstack-gate /opt/stack/new/devstack-gate
 
     echo "Attempting to adjust tempest.conf for libvirt-lxc"
-    sed -i 's/\#suspend\=true/#suspend=true\nsuspend=false/' /opt/stack/new/tempest/etc/tempest.conf
-    sed -i 's/\#rescue\=true/#rescue=true\nrescue=false/' /opt/stack/new/tempest/etc/tempest.conf
-    sed -i 's/\#resize\=false/#resize=false\nresize=false/' /opt/stack/new/tempest/etc/tempest.conf
-    sed -i 's/\#xml_api_v2\=true/#xml_api_v2=true\nxml_api_v2=false/' /opt/stack/new/tempest/etc/tempest.conf
-    sed -i 's/allow_tenant_isolation = True/allow_tenant_isolation = False/' /opt/stack/new/tempest/etc/tempest.conf
+    #sed -i 's/\#suspend\=true/#suspend=true\nsuspend=false/' /opt/stack/new/tempest/etc/tempest.conf
+    #sed -i 's/\#rescue\=true/#rescue=true\nrescue=false/' /opt/stack/new/tempest/etc/tempest.conf
+    #sed -i 's/\#resize\=false/#resize=false\nresize=false/' /opt/stack/new/tempest/etc/tempest.conf
+    #sed -i 's/\#xml_api_v2\=true/#xml_api_v2=true\nxml_api_v2=false/' /opt/stack/new/tempest/etc/tempest.conf
+    #sed -i 's/allow_tenant_isolation = True/allow_tenant_isolation = False/' /opt/stack/new/tempest/etc/tempest.conf
 
-    LNUM=`grep -n '\[network-feature-enabled\]\napi_extensions' tempest.conf | cut -d':' -f1`
+    #LNUM=`grep -n '\[network-feature-enabled\]\napi_extensions' tempest.conf | cut -d':' -f1`
 
-    sed -i "[network-feature-enabled]\napi_extensions = l3_agent_scheduler,agent,dhcp_agent_scheduler,router,allowed-address-pairs,extra_dhcp_opt/" "$(($LNUM))i" /opt/stack/new/tempest/etc/tempest.conf
-    sed -i 's/[network-feature-enabled]\napi_extensions = all/api_extensions = l3_agent_scheduler,agent,dhcp_agent_scheduler,router,allowed-address-pairs,extra_dhcp_opt/' /opt/stack/new/tempest/etc/tempest.conf
+    #sed -i "[network-feature-enabled]\napi_extensions = l3_agent_scheduler,agent,dhcp_agent_scheduler,router,allowed-address-pairs,extra_dhcp_opt/" "$(($LNUM))i" /opt/stack/new/tempest/etc/tempest.conf
+    #sed -i 's/[network-feature-enabled]\napi_extensions = all/api_extensions = l3_agent_scheduler,agent,dhcp_agent_scheduler,router,allowed-address-pairs,extra_dhcp_opt/' /opt/stack/new/tempest/etc/tempest.conf
 
-    sed -i 's/ force_tenant_isolation = True/# force_tenant_isolation = True/' /opt/stack/new/tempest/tempest/api/compute/servers/test_list_servers_negative.py
+    #sed -i 's/ force_tenant_isolation = True/# force_tenant_isolation = True/' /opt/stack/new/tempest/tempest/api/compute/servers/test_list_servers_negative.py
 
 }
 
