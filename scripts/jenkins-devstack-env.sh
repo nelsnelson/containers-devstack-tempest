7749c25d41ff0aa3a853c5b7f9e8cc0118e5f28b
Nels Nelson <nels.n.nelson@gmail.com>
2014-12-19 12:06:48 -0600

    Trying to disable tenant isolation

--- scripts/jenkins-devstack-env.sh
+++ scripts/jenkins-devstack-env.sh
@@ -12,5 +12,6 @@ export DEVSTACK_GATE_TIMEOUT=120
 export DEVSTACK_GATE_TEMPEST=1
 export DEVSTACK_GATE_TEMPEST_FULL=1
 export DEVSTACK_GATE_TEMPEST_DISABLE_TENANT_ISOLATION=true
+export NETWORK_API_EXTENSIONS=l3_agent_scheduler,agent,dhcp_agent_scheduler,router,allowed-address-pairs,extra_dhcp_opt
 export RE_EXEC=true
 
--- scripts/pre_test_hook.sh
+++ scripts/pre_test_hook.sh
@@ -3,24 +3,17 @@
 pre_test_hook() {
     echo "Invoked pre_test_hook"
     mkdir -p /opt/stack/new
+
     rm -rf /opt/stack/new/devstack-gate
     echo "Cloning https://github.com/nelsnelson/devstack-gate into /opt/stack/new/devstack-gate"
     git clone https://github.com/nelsnelson/devstack-gate /opt/stack/new/devstack-gate
 
-    echo "Attempting to adjust tempest.conf for libvirt-lxc"
-    #sed -i 's/\#suspend\=true/#suspend=true\nsuspend=false/' /opt/stack/new/tempest/etc/tempest.conf
-    #sed -i 's/\#rescue\=true/#rescue=true\nrescue=false/' /opt/stack/new/tempest/etc/tempest.conf
-    #sed -i 's/\#resize\=false/#resize=false\nresize=false/' /opt/stack/new/tempest/etc/tempest.conf
-    #sed -i 's/\#xml_api_v2\=true/#xml_api_v2=true\nxml_api_v2=false/' /opt/stack/new/tempest/etc/tempest.conf
-    #sed -i 's/allow_tenant_isolation = True/allow_tenant_isolation = False/' /opt/stack/new/tempest/etc/tempest.conf
-
-    #LNUM=`grep -n '\[network-feature-enabled\]\napi_extensions' tempest.conf | cut -d':' -f1`
-
-    #sed -i "[network-feature-enabled]\napi_extensions = l3_agent_scheduler,agent,dhcp_agent_scheduler,router,allowed-address-pairs,extra_dhcp_opt/" "$(($LNUM))i" /opt/stack/new/tempest/etc/tempest.conf
-    #sed -i 's/[network-feature-enabled]\napi_extensions = all/api_extensions = l3_agent_scheduler,agent,dhcp_agent_scheduler,router,allowed-address-pairs,extra_dhcp_opt/' /opt/stack/new/tempest/etc/tempest.conf
-
-    #sed -i 's/ force_tenant_isolation = True/# force_tenant_isolation = True/' /opt/stack/new/tempest/tempest/api/compute/servers/test_list_servers_negative.py
-
+    rm -rf /opt/stack/new/devstack
+    echo "Cloning https://github.com/nelsnelson/devstack into /opt/stack/new/devstack"
+    git clone https://github.com/nelsnelson/devstack /opt/stack/new/devstack
+    pushd /opt/stack/new/devstack
+    git checkout libvirt-lxc-tempest-customization2
+    popd
 }
 
 export -f pre_test_hook
